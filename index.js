ac3e5a10959d99bae1475f0e2d3745d637112b78
Richard <richardliang2015@u.northwestern.edu>
2025-04-18 10:45:55 -0400

    add zelle chase

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 const PORT = 8080;
+
+app.use(cors());
 
 // Serve everything inside provider-templates as static files
 app.use(express.static(path.join(__dirname, "/")));
